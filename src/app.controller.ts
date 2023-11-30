bc2a506e9ec6e2cf377009ee0149680bc958aae3
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2023-11-30 12:13:47 +0100

    Fix : Step 5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,5 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
   @Get("sayGoodbye")
-  sayGoodbye () {}
+  sayGoodbye () {
+    return this.appService.logicToSayGoodbye();
+  }
 }
