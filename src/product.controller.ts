eeef7ec5f16561399a08a8e039a9323f35cb4235
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2023-11-30 14:50:00 +0100

    Fix : Step 9

--- src/product.controller.ts
+++ src/product.controller.ts
@@ -1,7 +1,12 @@
-import { Controller} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller("products")
 export class ProductController {
-    constructor () {
+    constructor (private readonly productService: ProductService) {
+    }
+    @Get()
+    getProduct(): string[] {
+        return this.productService.getProduct();
     }
 }
