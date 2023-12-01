3fd078bab8a71ee922638f26338ef3a78538cd34
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2023-12-01 11:56:51 +0100

    Fix : add tracking user Step 26

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,8 +21,13 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  public async getByUUID(uuid: string) {
+    const trackUser = await this.prisma.users.findUnique({
+      where: {
+        UUID: uuid,
+      },
+    });
+    return trackUser;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
