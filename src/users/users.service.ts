190b1ad9221315b7702cde6ea6d2a5abc30afd78
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2023-12-01 15:36:36 +0100

    Fix : Normalize Step 29

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma.service';
+import NormalizedAnswer from 'src/normalized_answer';
 
 @Injectable()
 export class UsersService {
@@ -22,16 +23,21 @@ export class UsersService {
   }
 
   public async getByUUID(uuid: string) {
-    const trackUser = await this.prisma.users.findUnique({
+    const trackUser = new NormalizedAnswer(
+      `User ${uuid} has been found`, 
+      await this.prisma.users.findUnique({
       where: {
         UUID: uuid,
       },
-    });
+    }),
+    );
     return trackUser;
   }
 
   public async updateByUUID(uuid: string, updateUserDto: UpdateUserDto) {
-    const updatedUser = await this.prisma.users.update({
+    const updatedUser = new NormalizedAnswer(
+    `User ${updateUserDto.pseudo} has been updated`, 
+    await this.prisma.users.update({
       where: {
         UUID: uuid,
       },
@@ -39,16 +45,20 @@ export class UsersService {
         Pseudo: !!updateUserDto.pseudo ? updateUserDto.pseudo : undefined,
         Mail: !!updateUserDto.mail ? updateUserDto.mail : undefined,
       },
-    });
+    }),
+    );
     return updatedUser;
   }
 
   public async deleteUserByUUID (uuid : string){
-    const deleteUser = await this.prisma.users.delete({
+    const deleteUser = new NormalizedAnswer(
+      `User ${uuid} has been deleted`,
+      await this.prisma.users.delete({
       where: {
         UUID: uuid,
       },
-    });
+    }),
+    );
     return deleteUser;
   }
 }
