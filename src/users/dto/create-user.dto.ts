8d5d58bbebeacb0ea7ac23af13116f727f7e73f4
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2023-12-02 04:57:00 +0100

    Fix : Define user.dto Step 30

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsString, Length } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @Length(8, 50)
     pseudo : string;
+
+    @IsEmail()
+    @Length(10, 100)
     mail : string;
 }
