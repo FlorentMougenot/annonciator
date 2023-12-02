c82860e2aa465b9b827c61b07bda5f896c2b5769
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2023-12-02 05:58:14 +0100

    Fix : Init Swagger Step 34

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  appConfig(app)
+  appConfig(app);
+  initSwagger(app);
   await app.listen(3000);
 }
 function appConfig(app:INestApplication) {
   app.useGlobalPipes (new ValidationPipe({ transform : true }));
+}
+
+function initSwagger(app: INestApplication) {
+  SwaggerModule.setup(
+    'api',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder().addServer('E-Commerce').build(),
+    ),
+  );
 }
 bootstrap();
