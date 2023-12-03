bf0328f52610ff68a38df45fea2da21975018282
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2023-12-04 00:16:30 +0100

    Feat : Create Product Step 38

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller("products")
 export class ProductController {
     constructor (private readonly productService: ProductService) {
     }
+    @Post()
+    public create(@Body() CreateProductDto: CreateProductDto){
+        return this.productService.create(CreateProductDto);
+    }
     @Get()
     getProduct(): string[] {
-        return this.productService.getProduct();
+        return this.productService.getProducts();
     }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import NormalizedResponse from 'src/normalized_answer';
 
 @Injectable()
 export class ProductService {
-    getProduct(): string [] {
-        return [];
-    }
+  constructor(private readonly prisma: PrismaService) {}
+
+  getProducts(): string[] {
+    return ['Moto', 'Vélo', 'Voiture'];
+  }
+
+  public async create(createProductDto: CreateProductDto) {
+    return new NormalizedResponse(
+      `Product ${createProductDto.name} has been registered`,
+      await this.prisma.products.create({
+        data: {
+          Name: createProductDto.name,
+          Description: createProductDto.description,
+          Price: createProductDto.price,
+          Author: {
+            connect: {
+              UUID: createProductDto.owner_uuid,
+            },
+          },
+        },
+      }),
+    ).toJSON();
+  }
 }
