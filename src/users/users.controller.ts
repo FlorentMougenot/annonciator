f9c9acd1ac25c9db0721924addbe66dd76b5b058
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2023-12-04 00:49:32 +0100

    Fix : return Products from users Step 44

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ProductService } from 'src/product/product.service';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
 @ApiTags('Users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService, private readonly productsService: ProductService) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -22,6 +23,11 @@ export class UsersController {
   @Get(':uuid')
   public getByUUID(@Param('uuid') uuid: string) {
     return this.usersService.getByUUID(uuid);
+  }
+
+  @Get(':uuid/products')
+  public getProductsByUserUUID(@Param('uuid') uuid: string) {
+    return this.productsService.getProductsByUserUUID(uuid);
   }
 
   @Patch(':uuid')
